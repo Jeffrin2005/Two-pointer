1b70931743f9077e4a042c1eaaf95b83bf7c62d6
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-07-21 15:13:39 +0530

    (TWO-POINTER)Interval List Intersections.cpp

---
+++ (TWO-POINTER)Interval List Intersections.cpp
@@ -0,0 +1,28 @@
+
+
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+        int pointer_a = 0, pointer_b = 0;
+        vector<vector<int>> result;
+        int n1 = A.size();
+        int n2 = B.size();
+        while (pointer_a < n1 && pointer_b < n2){
+            // Check if there is an overlap between A[pointer_a] and B[pointer_b]
+            if(B[pointer_b][0] <= A[pointer_a][1] && A[pointer_a][0] <= B[pointer_b][1]){
+                // Calculate the start and end of the intersection
+                int start = max(A[pointer_a][0], B[pointer_b][0]);
+                int end = min(A[pointer_a][1], B[pointer_b][1]);
+                result.push_back({start, end});
+            }
+            // Move the pointer that has the smaller endpoint, just increasing if the next interval will in the current highest range 
+            // so we incremnet the smallest
+            if(A[pointer_a][1] < B[pointer_b][1]){
+                pointer_a++;
+            }else{
+                pointer_b++;
+            }
+        }
+        return result;
+    }
+};
