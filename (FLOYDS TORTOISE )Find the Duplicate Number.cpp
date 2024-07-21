0b251de455314143792784e0ed114869fc5cfbc5
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-07-21 22:28:38 +0530

    (FLOYDS TORTOISE )Find the Duplicate Number.cpp

---
+++ (FLOYDS TORTOISE )Find the Duplicate Number.cpp
@@ -0,0 +1,22 @@
+// Floyd's Tortoise 
+// time comp  = o(n) 
+// space comp = o(n)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        // First phase to find intersection point
+        do {
+            slow = nums[slow];      // Move slow by 1 step
+            fast = nums[nums[fast]]; // Move fast by 2 steps
+        } while(slow != fast);
+        // Second phase to find the entry point of the cycle
+        slow = nums[0]; // Reset slow to the start
+        while (slow != fast) {
+            slow = nums[slow]; // Move slow by 1 step
+            fast = nums[fast]; // Move fast by 1 step
+        }
+        return fast; 
+    }
+};
