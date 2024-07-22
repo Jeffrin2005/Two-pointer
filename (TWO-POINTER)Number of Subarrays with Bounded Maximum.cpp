e9fb5687aa7311555d5c8f065f9807e63dacbf8d
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-07-22 23:12:17 +0530

    (TWO-POINTER)Number of Subarrays with Bounded Maximum.cpp

---
+++ (TWO-POINTER)Number of Subarrays with Bounded Maximum.cpp
@@ -0,0 +1,21 @@
+
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right){
+        int count = 0, leftIndex = -1, lastValidIndex = -1;
+        // lastvalidexIndex means the end 
+        for (int i = 0; i < nums.size(); ++i) {// dry run this example  [2,9,2,5,6]
+            if (nums[i] > right){// if 9 greater that right means it is not satisfying the condtion so valid index will be starting from ther updated to leftindex = i 
+                leftIndex = i;  // dry run this example  [2,9,2,5,6]
+                // lastValidIndex is not updated because nums[1] is out of range. 
+            }
+            if (nums[i] >= left && nums[i] <= right) {
+                lastValidIndex = i;
+            }
+            if (lastValidIndex > leftIndex){
+                count += (lastValidIndex - leftIndex);
+            }
+        }
+        return count;
+    }
+};
