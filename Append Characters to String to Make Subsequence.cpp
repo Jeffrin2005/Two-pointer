aae677e5ac27fcf93e7b8accb424ba776bee48f6
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-08-16 21:36:08 +0530

    Create Append Characters to String to Make Subsequence.cpp

---
+++ Append Characters to String to Make Subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+         int i = 0;
+         int  j = 0;
+    int len_s = s.size();
+    int  len_t = t.size();
+    while (i < len_s && j < len_t) {
+        if(s[i] == t[j]){
+            j++;
+        }
+        i++;
+    }
+    return len_t - j;
+    }
+};
