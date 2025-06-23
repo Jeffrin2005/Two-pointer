8a5d643bf45640c541fe5d734da6cb8a1ee62d23
jevinjojo <jevinjojo1@gmail.com>
2025-06-23 18:29:17 +0530

    temp5

--- (BRUTEFORCE)Number of Arithmetic Triplets.cpp
+++ (BRUTEFORCE)Number of Arithmetic Triplets.cpp
@@ -8,6 +8,8 @@ public:
       // space complexity = o(1)
       for(int i=0; i < n; i++){
           for(int j = i + 1;j < n; j++){
+
+            
               for(int k = j + 1; k < n; k++){
                   if(nums[k] - nums[j] == diff && nums[j] - nums[i] == diff){
                       cnt++;
