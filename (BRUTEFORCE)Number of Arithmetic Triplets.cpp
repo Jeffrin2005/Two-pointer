8a414735efab7e4fe19a139ba4d2f09186fdb3e5
Jeffrin2005 <jeffrinjojo1@gmail.com>
2025-06-23 18:31:29 +0530

    temp6

--- (BRUTEFORCE)Number of Arithmetic Triplets.cpp
+++ (BRUTEFORCE)Number of Arithmetic Triplets.cpp
@@ -9,6 +9,11 @@ public:
       for(int i=0; i < n; i++){
           for(int j = i + 1;j < n; j++){
 
+
+
+
+
+            
             
               for(int k = j + 1; k < n; k++){
                   if(nums[k] - nums[j] == diff && nums[j] - nums[i] == diff){
