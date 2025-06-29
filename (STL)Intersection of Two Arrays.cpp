c17a060213741021e0885d66e140939028a7120f
Jeffrin2005 <jeffrinjojo1@gmail.com>
2025-06-29 18:54:46 +0530

    temp1

--- (STL)Intersection of Two Arrays.cpp
+++ (STL)Intersection of Two Arrays.cpp
@@ -5,6 +5,7 @@ public:
 Time complexity:o(2nlogn)
 Space complexity:o(n)
 */
+// testing purpose
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         set<int> set1(nums1.begin(), nums1.end());
         set<int> resultSet;
