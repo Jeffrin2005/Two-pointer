d8c234956014c1320c6b02a08f6ea70f19fe1d34
Jeffrin2005 <jeffrinjojo1@gmail.com>
2025-07-01 08:07:35 +0530

    rambo

--- (STL)Intersection of Two Arrays.cpp
+++ (STL)Intersection of Two Arrays.cpp
@@ -1,21 +1,2 @@
 
-class Solution {
-public:
-/*
-
-// testing purpose
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        set<int> set1(nums1.begin(), nums1.end());
-        set<int> resultSet;
-        vector<int> result;
-        for(auto&num : nums2){
-            
-            if (set1.find(num) != set1.end()) {
-                resultSet.insert(num);
-            }
-        }
-//resultSet to vector
-        result.assign(resultSet.begin(), resultSet.end());
-        return result;
-    }
-};
+// testign 
